1ba14b11e6a358f513319a85fef5118ea7a845a1
Lars With <lars@with.de>
2022-12-11 17:03:35 +0100

    remove TestAccFile_read_content_sensitive

--- internal/provider/data_file_test.go
+++ internal/provider/data_file_test.go
@@ -66,51 +66,6 @@ func TestAccDataFile_read_content(t *testing.T) {
 	})
 }
 
-func TestAccFile_read_content_sensitive(t *testing.T) {
-	testConfig := newTestFileConfig()
-
-	acctest.Current().Targets.Foreach(t, func(t *testing.T, target acctest.Target) {
-		t.Parallel()
-
-		resource.Test(t, resource.TestCase{
-			ProviderFactories: acctest.ProviderFactories(),
-			Steps: []resource.TestStep{
-				{
-					Config: tfbuild.FileString(tfbuild.File(
-						acctest.ProviderConfigBlock(target.Configs.Default()),
-						testAccFileBlock("test_sensitive", testRunFilePath(target, testConfig.fileName),
-							tfbuild.AttributeString("mode", "644"),
-							tfbuild.AttributeString("content_sensitive", "hello s3cr3t!"),
-						),
-						testAccDataFileBlock("test_sensitive", testRunFilePath(target, testConfig.fileName)),
-					)),
-					Check: resource.ComposeTestCheckFunc(
-						resource.TestCheckResourceAttr("system_file.test_sensitive", "id", testRunFilePath(target, testConfig.fileName)),
-						resource.TestCheckResourceAttr("system_file.test_sensitive", "path", testRunFilePath(target, testConfig.fileName)),
-						resource.TestCheckResourceAttr("system_file.test_sensitive", "mode", "644"),
-						resource.TestCheckResourceAttr("system_file.test_sensitive", "user", "root"),
-						resource.TestCheckResourceAttr("system_file.test_sensitive", "uid", "0"),
-						resource.TestCheckResourceAttr("system_file.test_sensitive", "group", "root"),
-						resource.TestCheckResourceAttr("system_file.test_sensitive", "gid", "0"),
-						// echo -n 'hello s3cr3t!' | openssl dgst -binary -md5 | openssl base64
-						resource.TestCheckResourceAttr("system_file.test_sensitive", "md5sum", "su6hNsFeImKJgWRpqtfzZw=="),
-
-						resource.TestCheckResourceAttr("data.system_file.test_sensitive", "id", testRunFilePath(target, testConfig.fileName)),
-						resource.TestCheckResourceAttr("data.system_file.test_sensitive", "path", testRunFilePath(target, testConfig.fileName)),
-						resource.TestCheckResourceAttr("data.system_file.test_sensitive", "mode", "644"),
-						resource.TestCheckResourceAttr("data.system_file.test_sensitive", "user", "root"),
-						resource.TestCheckResourceAttr("data.system_file.test_sensitive", "uid", "0"),
-						resource.TestCheckResourceAttr("data.system_file.test_sensitive", "group", "root"),
-						resource.TestCheckResourceAttr("data.system_file.test_sensitive", "gid", "0"),
-						// echo -n 'hello s3cr3t!' | openssl dgst -binary -md5 | openssl base64
-						resource.TestCheckResourceAttr("data.system_file.test_sensitive", "md5sum", "su6hNsFeImKJgWRpqtfzZw=="),
-					),
-				},
-			},
-		})
-	})
-}
-
 func testAccDataFileBlock(name string, path string, attrs ...tfbuild.BlockElement) tfbuild.FileElement {
 	resourceAttrs := []tfbuild.BlockElement{
 		tfbuild.AttributeString("path", path),
